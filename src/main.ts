c460ace7770b034f531ca63df1e2e3a7759559a7
Zen Software <ZenSoftware@users.noreply.github.com>
2019-09-08 20:55:47 -0700

    basic tslint and prettier config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import * as express from "express";
+import * as express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
 const PORT = process.env.PORT || 3000;
