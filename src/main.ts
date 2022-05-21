5e844e9954eb19ab5dc53040f71b83d6fae3bbf0
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-20 19:43:41 -0600

    docker bridge network example

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,26 @@ import { Client } from 'pg';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
+app.get('/', async (req, res) => {
+  res.header('Content-Type', 'text/plain');
+  res.send(await sample());
 });
 
 async function sample() {
-  const client = new Client();
-  await client.connect();
-  const sampleQuery = await client.query('SELECT NOW()');
-  await client.end();
+  try {
+    const client = new Client();
+    await client.connect();
+    const sampleQuery = await client.query('SELECT NOW()');
+    await client.end();
 
-  console.log('sample', sampleQuery);
+    console.log('sample', sampleQuery);
+    return JSON.stringify(sampleQuery);
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-// sample();
+sample();
 
 const PORT = process.env.PORT || 3000;
 
