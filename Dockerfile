6a130d65bb2bc4a8cf066dc615a9bb840c3e3ac8
Zen Software <ZenSoftware@users.noreply.github.com>
2022-05-20 18:55:40 -0600

    docker-compose added

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,14 @@
 FROM node:16-alpine
 
-WORKDIR /usr/src/api
+WORKDIR /app
 
 COPY dist/ .
-
 COPY package*.json ./
+COPY .env .
+
 RUN npm ci --production
 
 ENV PORT=3000
 EXPOSE ${PORT}
 
-CMD [ "node", "main.js" ]
+CMD ["node", "main.js"]
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
+import 'dotenv/config';
 import express from 'express';
+import { Client } from 'pg';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
+
+async function sample() {
+  const client = new Client();
+  await client.connect();
+  const sampleQuery = await client.query('SELECT NOW()');
+  await client.end();
+
+  console.log('sample', sampleQuery);
+}
+
+// sample();
 
 const PORT = process.env.PORT || 3000;
 
