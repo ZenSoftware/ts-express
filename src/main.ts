7606f3d8a4a1a8dc06744803d2b4c5b281e592a9
Zen Software <ZenSoftware@users.noreply.github.com>
2022-06-19 12:43:27 -0600

    basic socket.io server

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-const PORT = process.env.PORT || 3000;
+io.on('connection', socket => {
+  console.log('a user connected');
+});
+
+const PORT = process.env.PORT || 3080;
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
